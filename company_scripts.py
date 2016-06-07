dbe29bc06b0ff9fb9db1d86dfe929592596abf80
Jonathan Frederick <jrfreder@gmail.com>
2016-06-07 16:04:59 -0400

    added messages for failed assertions

--- company_scripts.py
+++ company_scripts.py
@@ -1,10 +1,14 @@
+
+
 def print_result(info):
     """Takes in a dictionary with keys for 'company', 'title', 'url',
     and 'description' and prints them neatly to the terminal"""
 
     for key in ['company', 'title', 'url', 'description']:
-        assert key in info.keys()
-        assert isinstance(info[key], str)
+        assert key in info.keys(), \
+            "The key '{}' is not in the dictionary".format(key)
+        assert isinstance(info[key], str), \
+            "The value at '{}' is not a string".format(key)
 
     print('{} - {}'.format(info['company'], info['title']))
     print(info['url'])
