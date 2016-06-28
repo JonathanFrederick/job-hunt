92355e80f545a07bea4acd9638256853cf96b41f
Jonathan Frederick <jrfreder@gmail.com>
2016-06-28 00:59:21 -0400

    added string lengths

--- models.py
+++ models.py
@@ -8,9 +8,9 @@ class Listing(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String())
-    company = db.Column(db.String())
-    title = db.Column(db.String())
-    status = db.Column(db.String())
+    company = db.Column(db.String(32))
+    title = db.Column(db.String(128))
+    status = db.Column(db.String(10))
 
     def __init__(self, url, company, title):
         self.url = url
