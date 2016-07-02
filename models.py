662c700900808f47e03b7aae328751e516c40e8d
Jonathan Frederick <jrfreder@gmail.com>
2016-07-02 12:34:28 -0400

    added last_seen with function for updating

--- models.py
+++ models.py
@@ -12,6 +12,11 @@ class Listing(db.Model):
     title = db.Column(db.String(128))
     status = db.Column(db.String(10))
     scraped_dt = db.Column(db.DateTime())
+    last_seen = db.Column(db.DateTime())
+
+    def seen_now(self):
+        """Function for updating an entry on successive sightings"""
+        self.last_seen = datetime.now()
 
     def __init__(self, url, company, title):
         self.url = url
@@ -19,6 +24,7 @@ class Listing(db.Model):
         self.title = title
         self.status = "NEW"
         self.scraped_dt = datetime.now()
+        self.last_seen = datetime.now()
 
     def __repr__(self):
         return '{} : {}'.format(self.company, self.title)
